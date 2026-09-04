90ca819f0e1d5989b96ffe06a987752ad44027b0
niyaj-seikh-2003 <niyazshkk@gmail.com>
2026-09-04 17:53:28 +0530

    commit

---
+++ BootDataJpaProj02-PagingAndSorting/src/main/java/com/nt/repository/IHospitalRepository.java
@@ -0,0 +1,10 @@
+package com.nt.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.nt.entity.Hospital;
+
+public interface IHospitalRepository extends CrudRepository<Hospital, Integer>,PagingAndSortingRepository<Hospital,Integer> {
+
+}
